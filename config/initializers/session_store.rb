49177b849d21972de9dc800cebcc9a3edaae0ed7
Eric K. Kosgey <kosgeye@berea.edu>
2012-01-10 16:46:49 -0500

    Fixed session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-BooksBubble::Application.config.session_store :cookie_store, :key '_BooksBubble_session'
+BooksBubble::Application.config.session_store :cookie_store, key: '_BooksBubble_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
