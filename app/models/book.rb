7d3bccc562d8217635da19aa49e2391a12760d52
Eric K. Kosgey <kosgeye@berea.edu>
2012-01-27 09:04:51 -0500

    Fixed CSS

--- app/models/book.rb
+++
@@ -1,4 +0,0 @@
-class Book < ActiveRecord::Base
-belongs_to :user
-default_scope :order => 'books.created_at DESC'
-end
--- app/models/item.rb
+++
@@ -1,9 +0,0 @@
-class Item < ActiveRecord::Base
-
-belongs_to :user
-
-validates :name, :presence => true, :length => { :maximum => 50 }
-validates :user_id, :presence => true
-  
-default_scope :order => 'items.created_at DESC'
-end
